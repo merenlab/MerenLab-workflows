f3744e7dd8f9a7b7b466acc4bdc96275cf026837
ShaiberAlon <alon.shaiber@gmail.com>
2017-06-14 20:57:25 -0500

    report whats running in test

--- assembly-based-metagenomics-workflow/test/run_merenlab_metagenomics_pipeline_test.sh
+++ assembly-based-metagenomics-workflow/test/run_merenlab_metagenomics_pipeline_test.sh
@@ -12,20 +12,20 @@ cp ../mock_files_for_merenlab_metagenomics_pipeline/*json $output_dir
 # we have to go into the test directory because snakemake requires you run the command from the directory where the snakemake is
 cd $output_dir
 
-# create samples.txt
+INFO "create samples.txt"
 echo -e "sample\tr1\tr2" > samples.txt
 echo -e "S01\tS01-QUALITY_PASSED_R1.fastq.gz\tS01-QUALITY_PASSED_R2.fastq.gz" >> samples.txt
 echo -e "S02\tS02-QUALITY_PASSED_R1.fastq.gz\tS02-QUALITY_PASSED_R2.fastq.gz" >> samples.txt
 
-# create empty fastq files
+INFO "create empty fastq files"
 mkdir 01_QC
 touch 01_QC/S01-QUALITY_PASSED_R1.fastq.gz 01_QC/S01-QUALITY_PASSED_R2.fastq.gz
 touch 01_QC/S02-QUALITY_PASSED_R1.fastq.gz 01_QC/S02-QUALITY_PASSED_R2.fastq.gz
 
-# Call snakefile
-snakemake --snakefile merenlab-metagenomics-pipeline.snakefile --cluster-config cluster.json --cluster 'clusterize -n {cluster.n} -log {cluster.log}' --jobs 4 --latency-wait 100 -np 
+INFO "Call snakefile"
+snakemake --snakefile merenlab-metagenomics-pipeline.snakefile --cluster-config cluster.json --cluster 'clusterize -n {threads} -log {cluster.log}' --jobs 4 --latency-wait 100 -np 
 
-# clear all files
+INFO "clear all files"
 rm *.snakefile config.json cluster.json samples.txt
 rm -rf 00_LOGS 01_QC
 
